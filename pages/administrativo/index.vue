1dbeb13c58ee9839cd584b9a2ffc136d56a9dd93
RafaelFrz <rafael.ferraz@unochapeco.edu.br>
2022-08-17 18:26:51 -0300

    primeiro commit

---
+++ pages/administrativo/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <v-btn
+    to="/create"
+  >
+    Create
+  </v-btn>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ pages/create.vue
@@ -0,0 +1,17 @@
+<template>
+  <v-container style="background-color: red;">
+    <v-btn>
+        Novo
+    </v-btn>
+  </v-container>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
